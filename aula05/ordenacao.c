98c20be4fd3f26dd0f1ae2889777b324e22ccc9a
Guilherme-Martins2311 <guilhermemartins2311@gmail.com>
2025-09-16 12:28:38 -0300

    a

---
+++ aula05/ordenacao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void TrocarElemento(int * A, int * B){
+
+    // Obs: A e B sao ponteiros (guardam endereços de memória)
+
+    int temp = *A;
+    *A = *B;
+    *B = temp;
+}
+
+int Particao(int * V, int Inf, int Sup){
+
+}
+
+int main() {
+
+}
